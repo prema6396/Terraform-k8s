2ab7c0ed4b4c381acb2e31af2b930673db7d9f4b
prema6396 <131760681+prema6396@users.noreply.github.com>
2024-02-06 21:21:56 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.15.0"
+    }
+  }
+}
+
+provider "google" {
+ 
+  credentials = file("/path/to/your/service-account-key.json")  # Replace with the actual path to your service account JSON key file
+  project     = "encoded-stage-408805"                               # Replace with your Google Cloud project ID
+  region      = "us-central1"                                    # Replace with your desired region, e.g., "us-central1"
+
+}
+resource "google_container_cluster" "my_cluster" {
+  name     = "my-gke-cluster"
+  location = "us-central1-a"                           # Replace with your desired GKE cluster location, e.g., "us-central1-a"
+
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+  }
+}
